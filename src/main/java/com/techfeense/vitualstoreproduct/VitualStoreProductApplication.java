e3c6363a0b42b0c8d8a32b66dec2dcee2cf48d9e
dayserivera <dayserivera@gmail.com>
2020-07-25 16:44:41 -0300

    first commit

---
+++ src/main/java/com/techfeense/vitualstoreproduct/VitualStoreProductApplication.java
@@ -0,0 +1,13 @@
+package com.techfeense.vitualstoreproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitualStoreProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitualStoreProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techfeense/vitualstoreproduct/VitualStoreProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techfeense.vitualstoreproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitualStoreProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
